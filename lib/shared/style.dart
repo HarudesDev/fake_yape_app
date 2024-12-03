3be898d779deaf47c2f19eb547a06af90324666b
HarudesDev <harudes.m@gmail.com>
2024-12-02 22:51:27 -0500

    fix: fixed app colors declaration

--- lib/shared/style.dart
+++ lib/shared/style.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 
-const mainColor = Color.fromRGBO(120, 49, 133, 1);
+const mainColor = Color.fromRGBO(115, 36, 131, 1);
 
 const mainColorDark = Color.fromRGBO(75, 25, 114, 1);
 
-const mainColorLight = Color.fromRGBO(143, 74, 153, 1);
+const mainColorLight = Color.fromRGBO(138, 61, 151, 1);
 
 const secondaryColor = Color.fromRGBO(0, 184, 180, 1);
 
