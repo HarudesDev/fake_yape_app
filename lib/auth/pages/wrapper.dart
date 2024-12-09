572479fc65730df52807202e3d5f865287cd5873
HarudesDev <harudes.m@gmail.com>
2024-12-08 21:22:17 -0500

    fix (auth): automatic change of pages on login and register

--- lib/auth/pages/wrapper.dart
+++ lib/auth/pages/wrapper.dart
@@ -1,5 +1,5 @@
 import 'package:fake_yape_app/auth/pages/welcome_page/welcome.dart';
-import 'package:fake_yape_app/home/pages/home.dart';
+import 'package:fake_yape_app/home/pages/home_page/home.dart';
 import 'package:fake_yape_app/shared/providers/auth_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:auto_route/auto_route.dart';
@@ -11,11 +11,10 @@ class Wrapper extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final authState = ref.watch(authProvider);
+    final authState = ref.watch(authStateChangeProvider);
 
     return authState.when(
         data: (user) {
-          print(AutoRouter.of(context).stack.toString());
           return user.session == null ? const WelcomePage() : const HomePage();
         },
         error: (error, _) => Text('Error:$error'),
